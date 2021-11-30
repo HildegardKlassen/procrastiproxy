6797c34c54f08fd74c776d64b3f344dc774a2249
Enrico Pau <enrico.pau@haevg-rz.de>
2021-11-30 18:46:29 +0100

    wip

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,14 +1,77 @@
 package proxy
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/http/httputil"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
-func Run() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe("localhost:8000", nil)
+func RunAllForbidden() {
+	http.ListenAndServe("localhost:9996", forbiddenRouter())
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func RunAllAllowed() {
+	http.ListenAndServe("localhost:9997", allowedRouter())
+}
+
+func RunBlockList() {
+	http.ListenAndServe("localhost:9998", blockListRouter())
+}
+
+func forbiddenHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusForbidden)
+}
+
+func allowedHandler(w http.ResponseWriter, r *http.Request) {
+	rp := httputil.NewSingleHostReverseProxy(r.URL)
+	rp.ServeHTTP(w, r)
+}
+
+func blockListHandler(w http.ResponseWriter, r *http.Request) {
+	blockList := parseBlockList()
+	for _, b := range blockList {
+		if strings.Contains(b, r.URL.Host) {
+			if strings.Contains(b, r.URL.Path) || (r.URL.Path == "/") {
+				w.WriteHeader(http.StatusForbidden)
+				return
+			}
+		}
+	}
+	rp := httputil.NewSingleHostReverseProxy(r.URL)
+	rp.Director = func(req *http.Request) {
+		req.URL = r.URL
+	}
+	//rp.Transport = &tls.Config{InsecureSkipVerify: true}
+	rp.ServeHTTP(w, r)
+}
+
+func forbiddenRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", forbiddenHandler)
+	return r
+}
+
+func allowedRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", allowedHandler)
+	return r
+}
+
+func blockListRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", blockListHandler)
+	r.PathPrefix("/").HandlerFunc(blockListHandler)
+	return r
+}
+
+func parseBlockList() []string {
+	content, err := ioutil.ReadFile("blocklist")
+	if err != nil {
+		return nil
+	}
+	lines := strings.Split(string(content), ",")
+	return lines
 }
--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -6,11 +6,17 @@ import (
 	"testing"
 )
 
-func Test_Proxy(t *testing.T) {
+const (
+	forbiddenProxyURL = "http://localhost:9996"
+	allowedProxyURL   = "http://localhost:9997"
+	blockListProxyURL = "http://localhost:9998"
+)
 
-	go func() { Run() }()
+func Test_ProxyEverythingAllowed(t *testing.T) {
 
-	pURL, _ := url.Parse("http://localhost:8000")
+	go RunAllAllowed()
+
+	pURL, _ := url.Parse(allowedProxyURL)
 
 	client := &http.Client{
 		Transport: &http.Transport{
@@ -22,7 +28,54 @@ func Test_Proxy(t *testing.T) {
 
 	if err != nil {
 		t.Error(err)
+	}
 
+	if res.StatusCode != 200 {
+		t.Error(err)
+	}
+
+}
+
+func Test_ProxyEverythingForbidden(t *testing.T) {
+
+	go RunAllForbidden()
+
+	pURL, _ := url.Parse(forbiddenProxyURL)
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(pURL),
+		},
+	}
+
+	res, err := client.Get("http://www.google.com")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != 403 {
+		t.Error(err)
+	}
+
+}
+
+func Test_ProxyBlockList(t *testing.T) {
+
+	go RunBlockList()
+
+	pURL, _ := url.Parse(blockListProxyURL)
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(pURL),
+		},
+	}
+
+	res, err := client.Get("http://www.google.com")
+
+	if err != nil {
+		t.Error(err)
 	}
 
 	if res.StatusCode != 403 {
